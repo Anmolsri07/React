e084e75d29b06a84ae9cec0a857cc918c630f225
Anmolsri07 <anmolsri001.dmk@gmail.com>
2024-01-09 19:27:37 +0530

    Enhansed TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,31 +6,61 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handeLowClick = () => {
+    console.log("upper case clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const clearClick = () => {
+    // console.log("upper case clicked" + text);
+    // let newText = text.toLowerCase();
+    setText(" ");
+  };
 
   const handleOnChange = (event) => {
     console.log("on change");
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("enter text here");
+  const [text, setText] = useState("");
   // text="new text" //wrong way to change state
   // setText("new text"); //correct wayto change state
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          class="form-control"
-          id="box"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            class="form-control"
+            id="box"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        {/* <p>you have clicked {count} times</p> */}
+
+        <button className="btn btn-primary mx-2" onClick={handeUpClick}>
+          Convert to upper case
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={handeLowClick}>
+          Convert to Lower case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={clearClick}>
+          Clear
+        </button>
       </div>
-      {/* <p>you have clicked {count} times</p> */}
-      <button className="btn btn-primary" onClick={handeUpClick}>
-        Convert to upper case
-      </button>
-    </div>
+
+      <div className="container my-3">
+        <h2>Enter yourText Summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} character
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
